868515daa6bad978f2a00f17adaa7d6e2a6c49ed
User2 <hyj_speechless@hotmail.com>
2021-01-08 00:41:50 +0800

    add component 2 to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Component1 from './Component/Component1';
+import Component2 from './Component/Component2';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         </p>
         <span>This is version 0</span>
         <Component1 />
+        <Component2 />
       </header>
     </div>
   );
